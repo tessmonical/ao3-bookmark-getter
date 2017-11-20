ee175f90ba7bc90fa0320bb04ef73d3901832a6d
Tess Monical <tess.monical@gmail.com>
2017-11-20 17:12:04 -0600

    asdfgh why doesn't it work

--- ao3downloader.py
+++ ao3downloader.py
@@ -1,10 +1,38 @@
 import urllib.request
+import urllib.parse
 import re
 import sys
 
 username = sys.argv[1] #your username
+password = ""
+if len(sys.argv) > 2:
+  password = sys.argv[2]
 currentPage = 1 #the page of bookmarks to start with
 end = False
+
+#login stuff
+if (password != ''):
+  url = 'http://archiveofourown.org/user_sessions/'
+  values = {
+    'user_session[login]': username,
+    'user_session[password]': password,
+    'user_session[remember_me]': 0,
+    'commit': 'Log in',
+    'utf8': u'✓',
+   }
+  data = urllib.parse.urlencode(values)
+  data = data.encode('ascii') # data should be bytes
+  req = urllib.request.Request(url, data)
+  with urllib.request.urlopen(req) as response:
+    the_page = response.read()
+    print(the_page)
+
+# url = 'http://archiveofourown.org/user_sessions/'
+# data = urllib.urlencode({username: username, password:password})
+# with urllib2.urlopen(url=url, data=data).read() as content:
+#   print(content)
+
+
 
 while not end:
   # gets the current page from your bookmarks
