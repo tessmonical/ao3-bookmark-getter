d4ad71fc63fe58891e0f1e5f52f221237724f29a
Tess Monical <tess.monical@gmail.com>
2017-11-21 09:52:30 -0600

    whyyy

--- ao3downloader.py
+++ ao3downloader.py
@@ -1,9 +1,6 @@
-import urllib.request
-import urllib.parse
 import re
 import sys
 import json
-import http.cookiejar
 import requests
 
 username = sys.argv[1] #your username
@@ -19,14 +16,13 @@ if (password != ''):
   # get an "authenticity token", which is an annoying thing that took me 100 years to figure out
   authenticity_token = {}
   url_token = 'http://archiveofourown.org/token_dispenser.json'
-
   jsonThing = requests.get(url_token)
   authenticity_token = jsonThing.json()['token']
   #print(authenticity_token)
 
 
   headers = {
-    "Content-Type":"application/x-www-form-urlencoded",
+    #"Content-Type":"application/x-www-form-urlencoded",
     "Accept":"text/html,*/*",
     "User-Agent":"ozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36",
     "Accept-Language":"en-US,en;q=0.9,ja;q=0.8",
@@ -37,30 +33,32 @@ if (password != ''):
   values = {
     'user_session[login]': username,
     'user_session[password]': password,
-    'user_session[remember_me]': 1,
+    'user_session[remember_me]': '1',
     #'commit': 'Log In',
     #'utf8': u'✓',
     'authenticity_token': authenticity_token #if you don't pass this along nothing works
   }
 
   the_page = requests.post(url,data=values,headers=headers)
-  print(the_page.text)
+  print(the_page.url)
+
 
 number_urls = 0
-while not end:
-  # gets the current page from your bookmarks
-  res = requests.get('http://archiveofourown.org/users/'+username+'/bookmarks?page='+str(currentPage))
-  html = res.text
-  # finds things that look like
-  # <a href="/works/#"> where # is a number
-  p = '<a href="\/works\/(\d+)">'
-  matches = re.findall(p, html)
-  #if there are no matches we've reached the end
-  if len(matches) < 1:
-      end = True
-  for match in matches:
-    print('http://archiveofourown.org/works/' + match)
-    number_urls += 1
-  currentPage += 1
+if (False):
+  while not end:
+    # gets the current page from your bookmarks
+    res = requests.get('http://archiveofourown.org/users/'+username+'/bookmarks?page='+str(currentPage))
+    html = res.text
+    # finds things that look like
+    # <a href="/works/#"> where # is a number
+    p = '<a href="\/works\/(\d+)">'
+    matches = re.findall(p, html)
+    #if there are no matches we've reached the end
+    if len(matches) < 1:
+        end = True
+    for match in matches:
+      print('http://archiveofourown.org/works/' + match)
+      number_urls += 1
+    currentPage += 1
 
 print("Finished! with "+str(number_urls)+" urls")
