1fbbc043327a293796df62de3ea45ef90495b20d
Tess Monical <tess.monical@gmail.com>
2017-11-28 11:30:10 -0600

    it logs in!!

--- ao3downloader.py
+++ ao3downloader.py
@@ -3,51 +3,51 @@ import sys
 import json
 import requests
 
+session = requests.Session()
+
 username = sys.argv[1] #your username
 password = ""
 if len(sys.argv) > 2:
   password = sys.argv[2]
 currentPage = 1 #the page of bookmarks to start with
 end = False
-cookies = {}
 
 #login stuff
 if (password != ''):
   # get an "authenticity token", which is an annoying thing that took me 100 years to figure out
   authenticity_token = {}
   url_token = 'http://archiveofourown.org/token_dispenser.json'
-  jsonThing = requests.get(url_token)
+  jsonThing = session.get(url_token)
+  # sessionCookieJar = jsonThing.cookies
+  # print("cookie: " + sessionCookieJar['_otwarchive_session'])
   authenticity_token = jsonThing.json()['token']
-  #print(authenticity_token)
+  # print('authenticity_token: '+ authenticity_token)
 
-
-  headers = {
-    #"Content-Type":"application/x-www-form-urlencoded",
-    "Accept":"text/html,*/*",
-    "User-Agent":"ozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36",
-    "Accept-Language":"en-US,en;q=0.9,ja;q=0.8",
-    "Connection":"keep-alive",
-    "Host":"archiveofourown.org"
-  }
-  url = 'http://archiveofourown.org/user_sessions/'
-  values = {
+  url = 'https://archiveofourown.org/user_sessions/'
+  data = {
     'user_session[login]': username,
     'user_session[password]': password,
-    'user_session[remember_me]': '1',
-    #'commit': 'Log In',
-    #'utf8': u'✓',
+    # 'user_session[remember_me]': 1,
     'authenticity_token': authenticity_token #if you don't pass this along nothing works
   }
+  headers = {
+    "Accept":"text/html,*/*",
+    "Host":"archiveofourown.org",
+    "Connection":"keep-alive",
+    "User-Agent":"ozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36",
+    "Content-Type":"application/x-www-form-urlencoded"
+  }
 
-  the_page = requests.post(url,data=values,headers=headers)
+  print('logging in as ' + username + ' with password ' + password)
+  the_page = session.post(url=url,data=data,headers=headers)
   print(the_page.url)
 
 
 number_urls = 0
-if (False):
+if (False): #not running this right now
   while not end:
     # gets the current page from your bookmarks
-    res = requests.get('http://archiveofourown.org/users/'+username+'/bookmarks?page='+str(currentPage))
+    res = session.get('http://archiveofourown.org/users/'+username+'/bookmarks?page='+str(currentPage))
     html = res.text
     # finds things that look like
     # <a href="/works/#"> where # is a number
